3c77b740b819e81570271994a55b450ab8f9d352
Mystery-Man2 <howell.thadj@gmail.com>
2024-12-02 17:02:46 -0600

    Assignment Finished

--- Module 1 - To Do Application.py
+++ Module 1 - To Do Application.py
@@ -73,24 +73,3 @@ else:
     print("\nHave a productive day!")
     print()
     exit()
-
-
-# ToDoApplication
-
-Module-1-To-Do-Assignment: This is my submission for the assignment at the end of Module 1.
-
-This is a fairly simple application to use.
-
-Upon opening the application, you will be met with a menu and the option to enter a selection.
-
-The menu only has 4 options: "1" to add a task. "2" to view all tasks. "3" to delete a task(s). "0" to end the program.
-
-** If you enter a word, sentence, or number that is not listed as an option on the main menu, the program will tell you to only enter the number for the selection you would like to execute.
-
-After selecting "1" from the main menu, it will prompt you to enter a task, which you will need to type out.
-
-The program will give you a confirmation that your task has been added to your to-do list and then it will bring you back to the menu and ask you for another selection. After selecting "2" from the main menu, it will show you all your tasks, listed in order starting with "1".
-
-After selecting "3" from the main menu, you will be presented with a choice to either clear the list entirely by entering "y" or to just delete 1 task by entering "n".
-
-If there are no tasks in your list, the application will not display the choice and will tell you that the list is empty and will instruct you to add a task before trying to delete one. If you select "y", the application will clear the list and delete ALL tasks, tell you it has done so, and then bring you back to the main menu. If you select "n", the application will ask you to enter the number of the task you wish to delete. Once you input the number of the task you wish to delete, the application will delete the task, tell you it has done so, and then return you to the main menu. If you attempt to type out the task while trying to delete it instead of using the number, or type in a number that is not attached to a task, the application will tell you that you must enter the number attached to the task to delete it and bring you back to the main menu. After selecting "0", the application will display the exit message and close.
